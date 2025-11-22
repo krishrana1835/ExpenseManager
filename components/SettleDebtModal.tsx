3a697ab1dd3af408c0454a349f5725f9d51d79a7
krishrana1835 <krishrana1835@gmail.com>
2025-11-22 11:28:53 +0530

    new

--- components/SettleDebtModal.tsx
+++ components/SettleDebtModal.tsx
@@ -80,7 +80,8 @@ const SettleDebtModal: React.FC<SettleDebtModalProps> = ({ isOpen, onClose, frie
                         {error && <p className="text-red-500 text-sm">{error}</p>}
                         <div className="flex justify-end space-x-3 pt-2">
                             <button type="button" onClick={onClose} className="px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-200 bg-gray-100 dark:bg-gray-600 rounded-md hover:bg-gray-200 dark:hover:bg-gray-500">Cancel</button>
-                            <button type="submit" disabled={!!error || !amount} className="px-4 py-2 text-sm font-medium dark:text-white bg-primary rounded-md hover:bg-primary-600 disabled:bg-red-500 disabled: text-white">
+                            <button type="submit" disabled={!!error || !amount} className="px-4 py-2 text-sm font-medium light:text-black dark:text-white bg-primary rounded-md outline-1 
+                            hover:bg-primary-600 disabled:bg-gray-400 disabled: text-white">
                                 {mode === 'pay' ? 'Pay Amount' : 'Receive Amount'}
                             </button>
                         </div>
