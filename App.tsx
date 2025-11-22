4990a71df1869bf0fc761010ddfe1ea1ebabc0cf
krishrana1835 <krishrana1835@gmail.com>
2025-11-22 11:40:00 +0530

    new

--- App.tsx
+++ App.tsx
@@ -220,7 +220,7 @@ const AppContent = () => {
 
           <button
             onClick={() => setIsModalOpen(true)}
-            className="w-16 h-16 -mt-8 bg-primary rounded-full text-white flex items-center justify-center shadow-xl hover:scale-110 transition-transform"
+            className="w-16 h-16 -mt-8 bg-primary rounded-full text-gray-600 dark:text-white flex items-center justify-center shadow-xl hover:scale-110 transition-transform"
           >
             <PlusIcon className="w-8 h-8" />
           </button>
